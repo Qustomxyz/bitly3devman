5ff3c8a63513e59806096597b4688a4accc66006
Vladimir <46341690+Qustomxyz@users.noreply.github.com>
2019-03-01 13:08:49 +0300

    Start new repository for dvmn Bitly course task3

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN_DEFAULT = os.getenv('TOKEN')
+
+
+def main():
+    user_link = input('Введите ссылку:')
+    link_data = get_link_counter(user_link)
+    if link_data:
+        answer = ["Короткая ссылка: {}".format(user_link), 'Количество переходов:']
+        for day_stat in link_data['link_clicks']:
+            answer.append('{} переходов: {}'.format(day_stat['date'][:10], day_stat['clicks']))      
+        print('\n'.join(answer))
+    else:
+        response = make_short_link(user_link)
+        if response:
+          print(response)
+        else:
+          print('Неправильный формат ссылки')
+
+
+def make_short_link(long_link, token=TOKEN_DEFAULT):
+    """
+    Функция делает короткую ссылку вида bit.ly/2GyKlVv из стандартного url
+    """
+    authorization = 'Bearer {}'.format(token)
+    headers = {
+        'Authorization': authorization,
+    }
+    params = {
+        'long_url': long_link
+    }
+    url = 'https://api-ssl.bitly.com/v4/bitlinks'
+    response = requests.post(url, json=params, headers=headers)
+    if response.ok:
+        link_tag = response.json()
+        return link_tag['link']
+    else:
+        return None
+
+
+def get_link_counter(bitlink, token=TOKEN_DEFAULT):
+    """
+    Функция возвращает статистику кликов по короткой ссылки за последний месяц (по дням)
+    """
+    authorization = 'Bearer {}'.format(token)
+    headers = {
+        'Authorization': authorization,
+    }
+    url = 'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks'.format(bitlink=bitlink)
+    response = requests.get(url, headers=headers)
+    if response.ok:
+        return response.json()
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    main()
